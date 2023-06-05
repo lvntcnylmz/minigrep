6b805e013d56138b0a5b0fbf467cd2b577a5fbf7
leventcanyilmaz <leventcanyilmaz@proton.me>
2023-06-05 10:59:05 +0300

    improve build and search with iterator

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,18 @@ impl Config {
     /// # Errors
     ///
     /// This function returns an error if there are not enough arguments.
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -61,14 +66,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 ///
 /// `search(query to search, file_path or file)`
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 /// Case-insensitive search for a query in a specified file.
@@ -77,16 +78,21 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 ///
 /// `search_case_insensitive(query to search, file_path or filez)`
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
+    // let query = query.to_lowercase();
+    // let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.to_lowercase().contains(&query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    // results
+
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use std::{env, process};
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         let error_message = "Problem parsing arguments.".bright_red();
         eprintln!("{}: {}", error_message, err.red().bold().blink());
         process::exit(1);
